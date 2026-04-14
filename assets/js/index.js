259f68d1cc7f56b297740763f5e5c54aae8bb721
Alejandro Di Stefano <addistefano@live.com.ar>
2026-04-14 20:57:00 -0300

    Clase 01

---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+// console.log("Hola desde el JS interno");
+
+// Nomenclatura camelCase
+/*
+var primeraVariable = "Hola"; // Creameos la variable (declaramos) y le asignamos un valor.
+primeraVariable = "CoderHouse" // Reasignacion de Variable
+primeraVariable = 5
+console.log(primeraVariable);
+
+var primeraVariable = "Hola soy Primera Variable con otro valor";
+console.log(primeraVariable);
+*/
+// ES6 -> Se dejo de utilizar var -> let
+/*
+let primeraVariable = 5; 
+primeraVariable = "CoderHouse"
+console.log(primeraVariable);
+
+let segundaVariable = "Hola soy Primera Variable con otro valor";
+console.log(segundaVariable);
+*/
+
+
+// Tipos de datos
+let terceraVariable = "Palabra"; // String
+terceraVariable = 1; // Number
+terceraVariable = true; // Boolean (true o false)
+terceraVariable = 'a'; // Caracter
+terceraVariable = "Varias palabras" // String
+terceraVariable = {
+    objeto: 1
+}; // Objeto
+terceraVariable = [1, 2, 3]; // Array
+
+// console.log(terceraVariable)
+
+// Operaciones matematicas
+
+let numeroA = 12;
+let numeroB = 10;
+console.log(numeroA + numeroB);
