3cbef2a98379c97e13b19ae7240cc8bf5552c1dd
Alejandro Di Stefano <addistefano@live.com.ar>
2026-05-05 20:53:26 -0300

    Clase 04

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,287 +1,255 @@
-// Clase 03
+// Unidad 4: Arrays y Objetos
 
-// Funciones
+// const miArrayDeNumeros = [1, 24, 83, 45, 145];
+// // Indices del Array   [0, 1, 2, 3, 4] -> Son los indices
+// console.log(miArrayDeNumeros);
+// console.log(typeof miArrayDeNumeros);
 
-// Diferencia entre let y const -> const no puede ser reasinar un valor, en cambio let si permite reasignaciones.
+// IMPORTANTE -> Los Arrays se definen usando const NUNCA let
+// miArrayDeNumeros = 5; // -> Se puede reasignar.
+// console.log(miArrayDeNumeros);
+// console.log(typeof miArrayDeNumeros);
 
-// const numeroA = 1;
-// const numeroB = 2;
+// Los Arrays tienen sus propios metodos (funciones).
 
-// const resultado = numeroA + numeroB;
+// Agregar un elemento al final de array
+// miArrayDeNumeros.push(100);
+// miArrayDeNumeros.push(56);
+// miArrayDeNumeros.push(65);
+// miArrayDeNumeros.push(99);
 
-// console.log("Felicitaciones, el resultado es: " + resultado);
+// console.log(miArrayDeNumeros);
+
+// Eliminar el ultimo elemento del Array
+// miArrayDeNumeros.pop(); // -> Saca el ultimo elemento
+// miArrayDeNumeros.pop();
+// miArrayDeNumeros.pop();
+// miArrayDeNumeros.pop();
+
+// console.log(miArrayDeNumeros);
 
 
-// const numeroC = 10;
-// const numeroD = 26;
+// Metodos destructivos -> Modifican los indices del Array
 
-// const resultado1 = numeroC + numeroD;
+// miArrayDeNumeros.shift(); // -> Elimina el 1er elemento del Array
+// console.log(miArrayDeNumeros);
+// miArrayDeNumeros.unshift(10);
+// console.log(miArrayDeNumeros);
 
-// console.log("Felicitaciones, el resultado es: " + resultado1);
+// console.log(miArrayDeNumeros.indexOf(0)); // En que indice se encuentra el Nro 0? -> Cuando no existe ese elemento me muestra -1
+// console.log(miArrayDeNumeros.indexOf(24)); // 1
+// console.log(miArrayDeNumeros.indexOf(145)); // 4
 
+// const miArrayDeNumeros = [1, 24, 83, 45, 145];
 
-// const numeroE = 105;
-// const numeroF = 246;
+// console.log(miArrayDeNumeros);
 
-// const resultado2 = numeroE + numeroF;
+// console.log("La cantidad de elementos de miArrayDeNumeros es: " + miArrayDeNumeros.length);
 
-// console.log("Felicitaciones, el resultado es: " + resultado2);
+// // Imprimir o Acceder a cualquier elemento a partir de su indice
 
-// KISS -> Keep is simple stupid
+// console.log(miArrayDeNumeros[2]); // Utilizando los [] accedemos a un valor del array segun su indice
+// console.log(miArrayDeNumeros[5]); // Cuando no existe un valor para ese indice me muestra "undefined"
 
-// Declarar una funcion que haga eso mismo pero mas simple:
-// Declaro variable global resultado
-// let resultado = 0;
-
-// function sumarDosNumerosProporcionadosPorElUsuario() {
-//     // Declaro variables locales
-//     const numeroA = parseFloat(prompt("Ingrese un numero A"));
-//     const numeroB = parseFloat(prompt("Ingrese un numero B"));
-//     // Resigno el valor de la variable resultado de 0 -> numeroA + numeroB
-//     resultado = numeroA + numeroB;
-//     // const resultado = numeroA + numeroB;
-//     console.log("Felicitaciones, el resultado es: " + resultado);
+// for (let i = 0; i < miArrayDeNumeros.length; i++) {
+//     console.log("Valor: " + miArrayDeNumeros[i], " para el indice " + i);
 // }
 
-// // Invocamos a la funcion
-// sumarDosNumerosProporcionadosPorElUsuario();
-// console.log("Felicitaciones, el resultado de la funcion es: " + resultado);
 
-// alert("Vamos de nuevo");
-// sumarDosNumerosProporcionadosPorElUsuario();
-// console.log("Felicitaciones, el resultado de la funcion es: " + resultado);
+// const miArrayDeNumeros = [1, 24, 83, 45, 145];
 
-// --------------------------------------
-// Declaro variable global resultado
-// let resultado = 0;
-// const numeroA = parseFloat(prompt("Ingrese un numero A"));
-// const numeroB = parseFloat(prompt("Ingrese un numero B"));
+// console.log(miArrayDeNumeros.join());
+// console.log(miArrayDeNumeros.join(" + "));
+// console.log(miArrayDeNumeros.join(" | "));
+// console.log(miArrayDeNumeros.join(" - "));
+// console.log(miArrayDeNumeros.join(" "));
 
-// function sumarDosNumerosProporcionadosPorElUsuario() {
-//     resultado = numeroA + numeroB;
-//     console.log("Felicitaciones, el resultado es: " + resultado);
-// }
-// function restarDosNumerosProporcionadosPorElUsuario() {
-//     resultado = numeroA - numeroB;
-//     console.log("Felicitaciones, el resultado es: " + resultado);
-// }
-// function multiplicarDosNumerosProporcionadosPorElUsuario() {
-//     resultado = numeroA * numeroB;
-//     console.log("Felicitaciones, el resultado es: " + resultado);
-// }
-// function dividirDosNumerosProporcionadosPorElUsuario() {
-//     resultado = numeroA / numeroB;
-//     if(numeroB !== 0){
-//         console.log("Felicitaciones, el resultado es: " + resultado);
-//     } else {
-//         console.error("Error, No se puede dividir por Cero");
-//     }
-// }
-// sumarDosNumerosProporcionadosPorElUsuario();
-// restarDosNumerosProporcionadosPorElUsuario();
-// multiplicarDosNumerosProporcionadosPorElUsuario();
-// dividirDosNumerosProporcionadosPorElUsuario();
-// sumarDosNumerosProporcionadosPorElUsuario();
-// sumarDosNumerosProporcionadosPorElUsuario();
-// sumarDosNumerosProporcionadosPorElUsuario();
-// sumarDosNumerosProporcionadosPorElUsuario();
-// sumarDosNumerosProporcionadosPorElUsuario();
+// console.log(miArrayDeNumeros.join(""));
+// console.log(miArrayDeNumeros.join("palabra"));
+
+// console.log(miArrayDeNumeros.indexOf(1)); // Retorna el indice del elemento
+// console.log(miArrayDeNumeros.includes(1)); // Retorna un boolean si esta o no el elemento
 
 
-// Funciones usando Parametros
-
-// Declaro variable global resultado
-// let resultado = 0;
-// const numeroA = parseFloat(prompt("Ingrese un numero A"));
-// const numeroB = parseFloat(prompt("Ingrese un numero B"));
-
-// function sumar(a, b) {
-//     resultado = a + b;
-//     console.log("Felicitaciones, el resultado es: " + resultado);
-// }
-// function restar(a, b) {
-//     resultado = a - b;
-//     console.log("Felicitaciones, el resultado es: " + resultado);
-// }
-// function multiplicar(a, b) {
-//     resultado = a * b;
-//     console.log("Felicitaciones, el resultado es: " + resultado);
-// }
-// function dividir(a, b) {
-//     resultado = a / b;
-//     if (b !== 0) {
-//         console.log("Felicitaciones, el resultado es: " + resultado);
-//     } else {
-//         console.error("Error, No se puede dividir por Cero");
-//     }
-// }
-
-// // Al momento de invocarlas, se le pasan los parametros.
-// sumar(numeroA, numeroB);
-// restar(numeroA, numeroB);
-// multiplicar(numeroA, numeroB);
-// dividir(numeroA, numeroB);
-
-// ---------------------------
-
-// Intentamos hacer una calculadora
-
-// let continuar = "si"; // Inicializamos para entrar al while
-
-// while (continuar.toLowerCase() !== "no") {
-
-//     const numeroA = parseFloat(prompt("Ingrese un numero A"));
-//     const numeroB = parseFloat(prompt("Ingrese un numero B"));
-//     const operacion = prompt("Ingrese la operacion.!");
-
-//     function calcular(a, b, op) {
-//         const mjeError = "Error, en la operacion.!";
-
-//         let resultado = 0;
-
-//         switch (op) {
-//             case "+":
-//                 resultado = a + b;
-//                 console.log("Felicitaciones, el resultado es: " + resultado);
-//                 break;
-//             case "-":
-//                 resultado = a - b;
-//                 console.log("Felicitaciones, el resultado es: " + resultado);
-//                 break;
-//             case "*":
-//                 resultado = a * b;
-//                 console.log("Felicitaciones, el resultado es: " + resultado);
-//                 break;
-//             case "/":
-//                 if (b !== 0) {
-//                     console.log("Felicitaciones, el resultado es: " + resultado);
-//                 } else {
-//                     console.error(mjeError);
-//                 }
-//                 break;
-//             default:
-//                 console.error("Error, Ingrese una operacion valida.!")
-//         }
-//     }
-
-//     calcular(numeroA, numeroB, operacion);
-
-//     continuar = prompt("¿Desea continuar? (Si/No)");
-// }
-
-// console.log("Gracias por usar la Calculadora.!!");
+// console.log(miArrayDeNumeros.indexOf(1000)); // -1
+// console.log(miArrayDeNumeros.includes(1000)); // false
 
 
-/// Funciones anonimas.!!
+// const miArrayDePalabras = ["Hola", "Mundo", "Coderhouse"];
+// console.log(miArrayDePalabras);
+// // for (let i = 0; i < miArrayDePalabras.length; i++) {
+// //     console.log("Valor: " + miArrayDePalabras[i], " para el indice " + i);
+// // }
 
-// const saludar = function (nombre) { // Siempre guardarlas en const
-//     return "Hola, " + nombre + ", ¿Como estas?"; // Retorne un String 
-// }
+// // console.log(miArrayDePalabras.includes("mundo")); // false
+// miArrayDePalabras.sort(); // Ordena alfabeticamente -> Cambia los indices (destructivo)
+// console.log(miArrayDePalabras);
 
-// console.log(saludar("Alejandro"));
+// miArrayDePalabras.sort().reverse(); // Ordena alfabeticamente pero invertido 
+// console.log(miArrayDePalabras);
 
-// // saludar = "Hola";
-// // console.log(saludar);
-
-// const sumar = function (a, b) {
-//     return a + b; // Evitamos declarar una nueva variable llamada resultado;
-// }
-
-// console.log(sumar(5, 6));
-// console.log(saludar("Sol"));
-// console.log(saludar("Gustavo"));
+// -----------------------------------------------------------------
 
 
-// Funcion Flecha -> Incorporada a partir ES9
+// Objetos
+
 /**
- * Las funciones flecha tienen un return implicito solo
- * cuando no son complejas o solo se pasa un parametro
+ * Formato del Objeto
+ * 
+ * const nombre = {
+ *      clave: valor,
+ *      clave: valor,
+ *      clave: valor,
+ * * }
  */
-//Ejemplos:
-// const saludar = (nombre) => "Hola " + nombre;
-// console.log(saludar("Alejandro"));
 
-// const sumarOConcatenar = (valorA, valorB) => valorA + valorB;
-
-// console.log(sumarOConcatenar("Alejandro", " Daniel"));
-// console.log(sumarOConcatenar(10, 55));
-
-
-// Creamos la calculadora con funcion flecha
-// const numeroA = parseFloat(prompt("Ingrese un numero A"));
-// const numeroB = parseFloat(prompt("Ingrese un numero B"));
-// const operacion = prompt("Ingrese la operacion.!");
-
-// const calcular = (a, b, op) => {
-//     switch (op) {
-//         case "+":
-//             return a + b;
-//         case "-":
-//             return a - b;
-//         case "*":
-//             return a * b;
-//         case "/":
-//             if (b === 0) {
-//                 return "Error, No se puede dividir por cero";
-//             } else {
-//                 return a / b;
-//             }
-//         default:
-//             return "Error, operacion no valida";
-//     }
+// const producto = {
+//     id: 1,
+//     nombre: "Azucar",
+//     precio: 1200,
+//     stock: 20
 // }
-// console.log(calcular(numeroA, numeroB, operacion));
+
+// // console.log(producto);
+// // console.log(typeof producto);
+
+// const productos = [
+//     {
+//         id: 1,
+//         nombre: "Azucar",
+//         precio: 1200,
+//         stock: 20
+//     },
+//     {
+//         id: 2,
+//         nombre: "Yerba",
+//         precio: 1950,
+//         stock: 15
+//     },
+//     {
+//         id: 3,
+//         nombre: "Coca Cola",
+//         precio: 3200,
+//         stock: 25
+//     },
+//     {
+//         id: 4,
+//         nombre: "Harina",
+//         precio: 1200,
+//         stock: 120
+//     }
+// ]
+
+// console.log(productos);
+// console.log(productos.length);
+// console.log(typeof productos);
 
 
-// Hacemos la misma calculadora de funcion flecha, pero usando un do while
 
-const calcular = (a, b, op) => {
-    switch (op) {
-        case "+":
-            return a + b;
-        case "-":
-            return a - b;
-        case "*":
-            return a * b;
-        case "/":
-            if (b === 0) {
-                return "Error, No se puede dividir por cero";
-            } else {
-                return a / b;
-            }
-        default:
-            return "Error, operacion no valida";
+// console.log(productos[0]);
+// console.log(productos[1].id);
+// console.log(productos[1].nombre);
+// console.log(productos[1].precio);
+// console.log(productos[1].stock);
+
+// productos.push(
+//     {
+//         id: 5,
+//         nombre: "Chocolate",
+//         precio: 9800,
+//         stock: 10
+//     }
+// )
+// console.log(productos);
+// console.log(productos.length);
+
+// const productoImportado = {
+//     id: 6,
+//     nombre: "Chocolate Importado",
+//     precio: 12900,
+//     stock: 50,
+//     origen: "Italia",
+//     codigo: "CODE123A"
+// };
+// productos.push(productoImportado);
+// productos.push("productoImportado"); // --> Se agrega como string
+
+
+// console.log(productos);
+// console.log(productos.length);
+
+// productos.push({
+//     id: 6,
+//     nombre: "Chocolate Suizo",
+//     precio: 18900,
+//     stock: 150,
+//     origen: "Suecia",
+//     codigo: "CODE456A",
+//     sabor: "Amargo"
+// })
+
+// console.log(productos);
+// console.log(productos.length);
+
+
+// const carrito = []; // Se define como array vacio
+
+// carrito.push(productos);
+
+// carrito.push(productos[0]);
+// carrito.push(productos[1]);
+// carrito.push(productos[4]);
+
+// carrito.push(productos[2],productos[3],productos[5]);
+// console.log(carrito);
+
+
+// Clases --> Siempre empieza en mayuscula
+
+class Producto {
+
+    constructor(id, nombre, precio, stock, origen, codigo, sabor) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+        this.origen = origen;
+        this.codigo = codigo;
+        this.sabor = sabor
     }
-}
 
-let continuar;
-
-do {
-    const validar = (mensaje) => {
-        let numero
-        do {
-            numero = parseFloat(prompt(mensaje));
-            if (isNaN(numero)) {
-                alert("Por favor, ingrese un numero valido")
-            }
-        } while (isNaN(numero));
-        return numero;
+    // Metodos
+    sumarIVA() {
+        this.precio = parseFloat((this.precio * 1.21).toFixed(2));
     }
-    // Usamos la funcion validar
-    const numeroA = validar("Ingrese un numero A");
-    const numeroB = validar("Ingrese un numero B");
-    const operacion = prompt("Ingrese la operacion.!");
+};
 
-    alert(calcular(numeroA, numeroB, operacion));
+const productosUsandoClases = [];
 
-    // Validamos lo que ingresa el usuario
-    do {
-        continuar = prompt("¿Desea continuar? (Si/No)").toLowerCase();
-        if (continuar !== "si" && continuar !== "no") {
-            alert("Por favor, ingrese 'Si' o 'No'");
-        }
-    } while (continuar !== "si" && continuar !== "no")
+// Utilizamos la clase Producto haciendo una instancia para poder usar su Constructor
+const primerProductoUsandoClase = new Producto(1, "Azucar", 1500.15, 20, "Argentina", "COD123A");
 
-} while (continuar === "si")
 
-alert("Gracias por usar la calculadora!");
+productosUsandoClases.push(primerProductoUsandoClase);
+productosUsandoClases.push(new Producto(2, "Yerba", 1800.55, 60, "Argentina", "YER1123")); // Si no agrego la propiedad lo muestra como undefined
+productosUsandoClases.push(new Producto(3, "Harina", 1350.50, 160, "Argentina", "HAR1123"));
+productosUsandoClases.push(new Producto(4, "Chocolate", 18500.99, 20, "Italia", "CHO1123", "Dulce"));
+
+// for (let i = 0; i < productosUsandoClases.length; i++) {
+//     console.log(productosUsandoClases[i])
+// }
+
+// productosUsandoClases.sumarIVA();
+// console.log(productosUsandoClases);
+
+// for (const producto of productosUsandoClases) {
+//     producto.sumarIVA();
+//     console.log(`El producto con ID: ${producto.id} es: ${producto.nombre}. - 
+// El Precio de ${producto.nombre}
+// es $${producto.precio}.- y existen en Stock: ${producto.stock} Und,
+// Origen: ${producto.origen}
+// Codigo: ${producto.codigo}
+// `);
+// }
+
+console.table(productosUsandoClases);
---
+++ ejercicios/ejercicios_05.md
@@ -0,0 +1,70 @@
+# Ejercicios de Arrays Combinados con Otros Conceptos
+
+#### Arrays y Objetos
+
+1. Crear una lista de objetos que representen personas, con propiedades `nombre` y `edad`.
+2. Añadir un nuevo objeto persona a la lista de personas.
+3. Eliminar el primer objeto persona de la lista de personas.
+4. Buscar una persona por su nombre en la lista de personas.
+5. Filtrar las personas mayores de 30 años en la lista.
+6. Ordenar la lista de personas por edad de manera ascendente.
+7. Calcular la edad promedio de las personas en la lista.
+8. Crear un array de nombres a partir de la lista de personas.
+9. Verificar si todas las personas en la lista son mayores de edad.
+10. Encontrar la persona más joven en la lista.
+
+#### Arrays y Clases
+
+1. Crear una clase `Persona` con propiedades `nombre` y `edad`.
+2. Instanciar un array de objetos `Persona` con diferentes nombres y edades.
+3. Añadir un método a la clase `Persona` que devuelva una cadena con el nombre y la edad.
+4. Crear una clase `Grupo` que contenga un array de objetos `Persona`.
+5. Implementar un método en la clase `Grupo` para añadir una nueva persona al grupo.
+6. Implementar un método en la clase `Grupo` para eliminar una persona por su nombre.
+7. Implementar un método en la clase `Grupo` para encontrar una persona por su nombre.
+8. Implementar un método en la clase `Grupo` para devolver todas las personas mayores de una cierta edad.
+9. Implementar un método en la clase `Grupo` para devolver el número total de personas en el grupo.
+10. Implementar un método en la clase `Grupo` para ordenar las personas por edad.
+
+#### Arrays y Constructores
+
+1. Crear una clase `Coche` con un constructor que acepte marca, modelo y año.
+2. Instanciar un array de objetos `Coche` con diferentes marcas, modelos y años.
+3. Implementar un método en la clase `Coche` que devuelva una cadena con la información del coche.
+4. Crear una clase `Concesionario` que contenga un array de objetos `Coche`.
+5. Implementar un método en la clase `Concesionario` para añadir un nuevo coche al concesionario.
+6. Implementar un método en la clase `Concesionario` para eliminar un coche por su modelo.
+7. Implementar un método en la clase `Concesionario` para encontrar un coche por su modelo.
+8. Implementar un método en la clase `Concesionario` para devolver todos los coches de una cierta marca.
+9. Implementar un método en la clase `Concesionario` para devolver el número total de coches en el concesionario.
+10. Implementar un método en la clase `Concesionario` para ordenar los coches por año.
+
+#### Arrays y `this`
+
+1. Crear una clase `Estudiante` con propiedades `nombre`, `nota1`, `nota2` y `nota3`.
+2. Implementar un método en la clase `Estudiante` que calcule la nota promedio usando `this`.
+3. Instanciar un array de objetos `Estudiante` con diferentes nombres y notas.
+4. Implementar un método en la clase `Estudiante` para determinar si el estudiante aprueba (nota promedio >= 6).
+5. Implementar un método en la clase `Estudiante` para devolver una cadena con el nombre y si aprueba o no.
+6. Crear una clase `Curso` que contenga un array de objetos `Estudiante`.
+7. Implementar un método en la clase `Curso` para añadir un nuevo estudiante al curso.
+8. Implementar un método en la clase `Curso` para eliminar un estudiante por su nombre.
+9. Implementar un método en la clase `Curso` para encontrar un estudiante por su nombre.
+10. Implementar un método en la clase `Curso` para devolver todos los estudiantes que aprueban.
+
+#### Arrays y Control de Flujos
+
+1. Crear un array de números y usar un bucle `for` para sumar todos los números.
+2. Crear un array de números y usar un bucle `for` para encontrar el número mayor.
+3. Crear un array de números y usar un bucle `for` para encontrar el número menor.
+4. Crear un array de números y usar un bucle `for` para contar cuántos números son mayores que 10.
+5. Crear un array de números y usar un bucle `for` para multiplicar todos los números por 2.
+6. Crear un array de números y usar un bucle `for` para crear un nuevo array con los números pares.
+7. Crear un array de números y usar un bucle `for` para crear un nuevo array con los números impares.
+8. Crear un array de números y usar un bucle `for` para calcular el promedio de los números.
+9. Crear un array de números y usar un bucle `for` para encontrar la suma de los números en posiciones pares.
+10. Crear un array de números y usar un bucle `for` para encontrar la suma de los números en posiciones impares.
+
+---
+
+## [Autor: Alejandro Di Stefano](https://github.com/Drako01)
